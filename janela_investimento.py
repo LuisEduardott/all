b57c46ac5b7d8c297e4f51d08cc6d96d4702fa8d
luis eduardo <juniorlluiseduardo@gmail.com>
2026-05-12 17:14:58 -0300

    final

--- janela_investimento.py
+++ janela_investimento.py
@@ -1,17 +1,16 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("Simulador de Investimentos - Sicredi")# nome da janela
+root.title(".exe")# nome da janela
 root.geometry("600x600")# resolucao
-root.configure(bg="#333")# cor do fundo
+root.configure(bg="#E31D2B")# cor do fundo
 root.resizable(width=False, height=False)#
 
-# Título
 titulo = tk.Label(
     root,
-    text="Simulador de Investimentos", # texto do titulo
-    font=("Arial", 25, "bold italic"),#fonte do text
-    bg="#333", #fundo do titulo
+    text="Santander", # texto do titulo
+    font=("Nunito", 50, "bold"),#fonte do text
+    bg="#E31D2B", #fundo do titulo
     fg="#fff" #cor do titulo
 )
 titulo.pack(anchor="center",pady=(20, 4))
@@ -20,11 +19,16 @@ titulo.pack(anchor="center",pady=(20, 4))
 valor_inicial = tk.Label(root,
 text= "Valor Inicial: ",
 font= ("Arial", 15, "bold"),
-bg="#333",
-fg="#fff"
-         
+bg="#E31D2B",
+fg="#fff"        
 )
 valor_inicial.pack(anchor="center",pady=(10,5))
+
+# Entrada de texto
+entrada_texto = tk.Entry(root, font=("Arial, 15"))
+entrada_texto.pack(side="left", padx=5)
+entrada_texto.configure(bg="#fff", fg="#000")
+
 
 root.mainloop()
 
